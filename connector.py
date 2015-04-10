c8d13f9d2965e9b647132fa8439f55338bf8914d
avoyta <artyom.voyta@gmail.com>
2015-04-10 11:34:44 +0300

    first commit

--- connector.py
+++ connector.py
@@ -1,9 +1,38 @@
-<<<<<<< HEAD
-__author__ = 'avoyta'
-=======
-__author__ = 'agorbachev'
 import tools.logger as logger
+import paramiko
+import time
+
 
 LOG = logger.getLogger('Connector')
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
->>>>>>> origin/master
+def run_cmd(host, user, passwd, cmd_list, client=ssh_client, port_number=22):
+    if type(cmd_list) is str:
+        cmd_list = [cmd_list]
+        try:
+            client.connect(
+                hostname=host,
+                port=port_number,
+                username=user,
+                password=passwd,
+                allow_agent=False)
+            shell = client.invoke_shell()
+            shell.recv(1024)
+            shell.send('environment no more\r\n')
+            time.sleep(0.5)
+            shell.recv(1024)
+        except:
+            print('Connection failed')
+        try:
+            output = {}
+            for cmd in cmd_list:
+                shell.send(cmd + '\r\n')
+                time.sleep(1)
+                output.update({cmd: '\n'.join(shell.recv(50000).decode('utf-8').split('\r\n')[1:])})
+            shell.close()
+            client.close()
+            return output
+        except UnboundLocalError:
+            print("Could'n create SHELL on node {0}".format(host))
+
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 __author__ = 'agorbachev'
 import tools.logger as logger
 import time
+import connector
 #TODO:add logging level as script parameter
 
-LOG = logger.getLogger('IntegrationTool')
-UserLOG = logger.getLogger('UserLOG')
-while True:
-    LOG.debug('Main logging test')
-    UserLOG.info('User log')
-    time.sleep(1)
+# LOG = logger.getLogger('IntegrationTool')
+# UserLOG = logger.getLogger('UserLOG')
+# while True:
+#     LOG.debug('Main logging test')
+#     UserLOG.info('User log')
+#     time.sleep(1)
+
+try:
+    for cmd, output in connector.run_cmd('172.17.14.95', 'admin', 'admin', '/admin save').items():
+        print('#', cmd)
+        print(output)
+except AttributeError:
+    print("Can't run command on remote node")
 
